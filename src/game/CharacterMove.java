9f872480a0d2b3ef6204c02124ced2fa961d77aa
RGCHN <dacong9610@163.com>
2019-04-09 15:48:17 +0800

    test

--- src/game/CharacterMove.java
+++ src/game/CharacterMove.java
@@ -31,22 +31,23 @@ public class CharacterMove {
 			default : break;
 		}
 		
-		BlockType moveType = nowField.get(x, y).cellType();
-		BlockType destType = nowField.get(nowX, nowY).cellType();
 		
-		//Current Character Move Result Set
-		nowField.place(x, y, new Cell(BlockType.EMPTY));
 		
 		if(!nowField.outOfBound(nowX, nowY)) {
+			BlockType moveType = nowField.get(x, y).cellType();
+			BlockType destType = nowField.get(nowX, nowY).cellType();
+			
+			//Current Character Move Result Set
+			nowField.place(x, y, new Cell(BlockType.EMPTY));
 			switch(destType) {
 				case STAR : {
 					nowField.place(nowX, nowY, new Cell(moveType));
 					return new ActionRes(nowField, moveType, Result.SCORE_NORMAL);
 				}
 				case SUP_STAR : {
-					if(moveType == BlockType.MONSTER) {
+					if(moveType.equals(BlockType.MONSTER)) {
 						nowField.place(nowX, nowY, new Cell(moveType));
-						return new ActionRes(nowField, moveType, Result.SCORE_SUPER);
+						return new ActionRes(nowField, BlockType.MONSTER, Result.SCORE_SUPER);
 					} else {
 						nowField.place(nowX, nowY, new Cell(BlockType.SUPER_PLAYER));
 						return new ActionRes(nowField, BlockType.SUPER_PLAYER, Result.SCORE_SUPER);
@@ -57,7 +58,7 @@ public class CharacterMove {
 					return new ActionRes(nowField, BlockType.EMPTY, Result.DEAD_HITWALL);
 				}
 				case MONSTER : {
-					if(moveType == BlockType.SUPER_PLAYER) {
+					if(moveType.equals(BlockType.SUPER_PLAYER)) {
 						nowField.place(nowX, nowY, new Cell(moveType));
 						return new ActionRes(nowField, moveType, Result.SCORE_KILLM);
 					} else if(moveType == BlockType.MONSTER) {
@@ -96,7 +97,7 @@ public class CharacterMove {
 					if(moveType==BlockType.MONSTER) {
 						return new ActionRes(nowField,BlockType.EMPTY, Result.DEAD_PLAYER);
 					} else if(moveType == BlockType.PLAYER1 || moveType == BlockType.PLAYER2) {
-						return new ActionRes(nowField,BlockType.MONSTER, Result.DEAD_PLAYER);
+						return new ActionRes(nowField,BlockType.EMPTY, Result.DEAD_PLAYER);
 					} else {
 						nowField.place(nowX, nowY, new Cell(moveType));
 						return new ActionRes(nowField, moveType, Result.NONE);
@@ -111,5 +112,47 @@ public class CharacterMove {
 			return new ActionRes(nowField, BlockType.EMPTY, Result.DEAD_OUT);
 		}
 	}
-	
+	public static void  main(String[] args) {
+		Field field=new Field(2,1);
+		Cell[] movers= {new Cell(BlockType.PLAYER1),new Cell(BlockType.PLAYER2),
+						new Cell(BlockType.SUPER_PLAYER),new Cell(BlockType.MONSTER)};
+		
+		Cell[] things= {new Cell(BlockType.STAR),new Cell(BlockType.SUP_STAR),
+						new Cell(BlockType.WALL),new Cell(BlockType.EMPTY)};
+		//测试角色遇到物品
+		for (int i=0;i<4;i++) {
+			for(int j=0;j<4;j++) {
+				field.place(0, 0, movers[i]);
+				field.place(1, 0, things[j]);
+				ActionRes aRes=CharacterMove.action(field, 0, 0, Action.MOVE_DOWN);
+				System.out.print("当"+movers[i].cellType()+"遇到"+things[j].cellType()+": ");
+				//依次返回移动后类型、得分、field情况
+				System.out.println(aRes.afterAction.toString()+" "+aRes.result.toString()+"  "+
+								   aRes.afterMove.get(0, 0).cellType()+" "+aRes.afterMove.get(1, 0).cellType());
+			}
+			System.out.println();
+		}
+		//角色遇到角色
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<4;j++) {
+				if(j!=i) {
+					field.place(0, 0, movers[i]);
+					field.place(1, 0, movers[j]);
+					ActionRes aRes=CharacterMove.action(field, 0, 0, Action.MOVE_DOWN);
+					System.out.print("当"+movers[i].cellType()+"遇到"+movers[j].cellType()+": ");
+					System.out.println(aRes.afterAction.toString()+" "+aRes.result.toString()+"  "+
+									   aRes.afterMove.get(0, 0).cellType()+" "+aRes.afterMove.get(1, 0).cellType());
+				}
+			}
+			System.out.println();
+		}
+		//越界测试
+		for(int i=0;i<4;i++) {
+			field.place(0, 0, movers[i]);
+			ActionRes aRes=CharacterMove.action(field, 0, 0, Action.MOVE_LEFT);
+			System.out.println(aRes.afterAction.toString()+" "+aRes.result.toString()+"  "+
+							   aRes.afterMove.get(0, 0).cellType()+" "+aRes.afterMove.get(1, 0).cellType());
+		}
+
+	}
 }
